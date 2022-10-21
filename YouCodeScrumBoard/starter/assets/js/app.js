23cabc056a4d5daf13835427bc0905f39c3c43bc
jahid oussama <jahid20oussama@gmail.com>
2022-10-21 17:10:17 +0100

    still not done

--- YouCodeScrumBoard/starter/assets/js/app.js
+++ YouCodeScrumBoard/starter/assets/js/app.js
@@ -96,7 +96,8 @@ function reloadTasks() {
     let cont_todo = 0,
         cont_inpr = 0,
         cont_done = 0,
-        cont_all = 0;
+        cont_all = 0,
+        icon = '';
 
     // Remove tasks elements
     document.querySelector("#to-do-tasks").innerHTML = '';
@@ -109,82 +110,53 @@ function reloadTasks() {
     const inp = document.querySelector("#in-progress-tasks");
 
     tasks.forEach((task) => {
+
+        let content = `<button class="w-100 border-0 " >
+        <div class="">
+            <i class=""></i> 
+        </div>
+        <div class="row"> 
+            <div class="col-1 py-3"><i class="bi ${icon} text-green fs-4"></i> </div>
+            <div class="col-11 py-3">
+        <div class="text-start">
+            <div class="fs-5 fw-bold">${task.title}</div>
+            <div class="">
+                <div class="fs-6 text-secondary ">#${cont_all} ${task.date}</div>
+                <div class="fs-6 d-inline-block text-truncate" title="${task.description}" style="max-width:16rem">${task.description}</div>
+            </div>
+            <div class="">
+                <span class="btn btn-primary btn-sm lh-1 rounded-pill">${task.priority}</span>
+                <span class="btn btn-secondary btn-sm lh-1 rounded-pill ">${task.type}</span>
+            </div>
+        </div>
+    </div>
+    </div>
+    </button>`;
+
         cont_all++;
         if (task.status === "To Do") {
+            icon = `spinner-border spinner-border-sm ms-10px`;
             cont_todo++;
-            Todo.innerHTML += `<button class="w-100 border-0 " >
-            <div class="">
-                <i class=""></i> 
-            </div>
-            <div class="row"> 
-                <div class="col-1 py-3"><i class="bi bi-question-circle text-green fs-4"></i> </div>
-                <div class="col-11 py-3">
-            <div class="text-start">
-                <div class="fs-5 fw-bold">${task.title}</div>
-                <div class="">
-                    <div class="fs-6 text-secondary ">#${cont_all} ${task.date}</div>
-                    <div class="fs-6 d-inline-block text-truncate" title="${task.description}" style="max-width:16rem">${task.description}</div>
-                </div>
-                <div class="">
-                    <span class="btn btn-primary btn-sm lh-1 rounded-pill">${task.priority}</span>
-                    <span class="btn btn-secondary btn-sm lh-1 rounded-pill ">${task.type}</span>
-                </div>
-            </div>
-        </div>
-        </div>
-        </button>`;
+            Todo.innerHTML += content;
 
         } if (task.status === "Done") {
-            dt.innerHTML += `<button class="w-100 border-0 " >
-        <div class="">
-            <i class=""></i> 
-        </div>
-        <div class="row"> 
-            <div class="col-1 py-3"><i class="bi bi-question-circle text-green fs-4"></i> </div>
-            <div class="col-11 py-3">
-        <div class="text-start">
-            <div class="fs-5 fw-bold">${task.title}</div>
-            <div class="">
-                <div class="fs-6 text-secondary ">#${cont_all} ${task.date}</div>
-                <div class="fs-6 d-inline-block text-truncate" title="${task.description}" style="max-width:16rem">${task.description}</div>
-            </div>
-            <div class="">
-                <span class="btn btn-primary btn-sm lh-1 rounded-pill">${task.priority}</span>
-                <span class="btn btn-secondary btn-sm lh-1 rounded-pill ">${task.type}</span>
-            </div>
-        </div>
-    </div>
-    </div>
-    </button>`;
+            icon = `spinner-border spinner-border-sm ms-10px`;
+            dt.innerHTML += content;
         } if (task.status === "In Progress") {
-            inp.innerHTML += `<button class="w-100 border-0 " >
-        <div class="">
-            <i class=""></i> 
-        </div>
-        <div class="row"> 
-            <div class="col-1 py-3"><i class="bi bi-question-circle text-green fs-4"></i> </div>
-            <div class="col-11 py-3">
-        <div class="text-start">
-            <div class="fs-5 fw-bold">${task.title}</div>
-            <div class="">
-                <div class="fs-6 text-secondary ">#${cont_all} ${task.date}</div>
-                <div class="fs-6 d-inline-block text-truncate" title="${task.description}" style="max-width:16rem">${task.description}</div>
-            </div>
-            <div class="">
-                <span class="btn btn-primary btn-sm lh-1 rounded-pill">${task.priority}</span>
-                <span class="btn btn-secondary btn-sm lh-1 rounded-pill ">${task.type}</span>
-            </div>
-        </div>
-    </div>
-    </div>
-    </button>`;
+            icon = `spinner-border spinner-border-sm ms-10px`;
+            inp.innerHTML += content;
         }
+
+
 
 
 
 
     })
     document.getElementById("to-do-tasks-count").innerText = cont_todo;
+
+    // to clean the form
+    document.getElementById('form').reset();
 
     // Set Task count
 }
