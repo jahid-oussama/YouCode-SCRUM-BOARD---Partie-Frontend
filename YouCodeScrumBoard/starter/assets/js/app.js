20cbe47fb9a9878dfb4570d287e31972b30f809e
jahid oussama <jahid20oussama@gmail.com>
2022-10-24 14:38:06 +0100

    i finished all

--- YouCodeScrumBoard/starter/assets/js/app.js
+++ YouCodeScrumBoard/starter/assets/js/app.js
@@ -9,6 +9,20 @@ function createTask() {
     // Afficher le boutton save
 
     // Ouvrir modal form
+
+    document.getElementById("save").style.display = "block"
+    document.getElementById("update").style.display = "none"
+
+}
+
+function saveTask() {
+    // Recuperer task attributes a partir les champs input
+
+    // Créez task object
+
+    // Ajoutez object au Array
+
+    // refresh tasks
     const form = document.getElementById("form");
     form.addEventListener('submit', (e) => {
 
@@ -35,35 +49,38 @@ function createTask() {
 
     })
 
-
-}
-
-function saveTask() {
-    // Recuperer task attributes a partir les champs input
-
-    // Créez task object
-
-    // Ajoutez object au Array
-
-    // refresh tasks
-
 }
 
 function editTask(index) {
     // Initialisez task form
-
+    $("#modal-task").modal("show")
     // Affichez updates
+    console.log(tasks[index - 1])
+    document.getElementById("title").value = tasks[index - 1].title;
+    document.getElementById("priority").value = tasks[index - 1].priority;
+    document.getElementById("status").value = tasks[index - 1].status;
+    document.getElementById("date").value = tasks[index - 1].date;
+    document.getElementById("description").value = tasks[index - 1].description;
+    if (tasks[index - 1].type == "Bug") {
+        document.getElementById("Bug").checked = true
 
+    } else {
+        document.getElementById("Feature").checked = true
+    }
+    document.getElementById("save").style.display = "none"
+    document.getElementById("update").style.display = "block"
     // Delete Button
 
+    document.getElementById("update").setAttribute("onclick", "updateTask(" + (index - 1) + ")")
     // Définir l’index en entrée cachée pour l’utiliser en Update et Delete
 
     // Definir FORM INPUTS
 
     // Ouvrir Modal form
+
 }
 
-function updateTask() {
+function updateTask(index) {
     // GET TASK ATTRIBUTES FROM INPUTS
 
     // Créez task object
@@ -74,9 +91,27 @@ function updateTask() {
 
     // Refresh tasks
 
+    var title = document.getElementById("title").value;
+    var priority = document.getElementById("priority").value;
+    var status = document.getElementById("status").value;
+    var date = document.getElementById("date").value;
+    var description = document.getElementById("description").value;
+    var type = document.querySelector('input[name="type"]:checked').value;
+    var task = {
+        title: title,
+        priority: priority,
+        status: status,
+        date: date,
+        description: description,
+        type: type
+    }
+    tasks[index] = task
+    reloadTasks()
+
+
 }
 
-function deleteTask() {
+function deleteTask(index) {
     // Get index of task in the array
 
     // Remove task from array by index splice function
@@ -84,6 +119,10 @@ function deleteTask() {
     // close modal form
 
     // refresh tasks
+
+    tasks.splice(index - 1, 1);
+    reloadTasks();
+
 }
 
 function initTaskForm() {
@@ -110,13 +149,30 @@ function reloadTasks() {
     const inp = document.querySelector("#in-progress-tasks");
 
     tasks.forEach((task) => {
+        cont_all++;
+
+        if (task.status === "To Do") {
+            icon = `bi bi-question-circle-fill`;
+
+        } if (task.status === "Done") {
+            icon = `bi bi-check2-square`;
+
+        } if (task.status === "In Progress") {
+            icon = `spinner-border spinner-border-sm `;
+
+        }
+
+
 
         let content = `<button class="w-100 border-0 " >
         <div class="">
             <i class=""></i> 
         </div>
         <div class="row"> 
-            <div class="col-1 py-3"><i class="bi ${icon} text-green fs-4"></i> </div>
+            <div class="col-1 py-3"><i class="bi ${icon} text-green fs-4"></i>
+            <i class="bi bi-pencil-square text-primary fs-4" onclick="editTask(${cont_all})"></i>
+            <i id="${cont_all}" class="bi bi-x-circle-fill text-danger fs-4" onclick="deleteTask(${cont_all})"></i> 
+            </div>
             <div class="col-11 py-3">
         <div class="text-start">
             <div class="fs-5 fw-bold">${task.title}</div>
@@ -133,17 +189,16 @@ function reloadTasks() {
     </div>
     </button>`;
 
-        cont_all++;
+
         if (task.status === "To Do") {
-            icon = `spinner-border spinner-border-sm ms-10px`;
             cont_todo++;
             Todo.innerHTML += content;
 
         } if (task.status === "Done") {
-            icon = `spinner-border spinner-border-sm ms-10px`;
+            cont_done++;
             dt.innerHTML += content;
         } if (task.status === "In Progress") {
-            icon = `spinner-border spinner-border-sm ms-10px`;
+            cont_inpr++;
             inp.innerHTML += content;
         }
 
@@ -154,6 +209,8 @@ function reloadTasks() {
 
     })
     document.getElementById("to-do-tasks-count").innerText = cont_todo;
+    document.getElementById("in-progress-tasks-count").innerText = cont_inpr;
+    document.getElementById("done-tasks-count").innerText = cont_done;
 
     // to clean the form
     document.getElementById('form').reset();
@@ -161,4 +218,4 @@ function reloadTasks() {
     // Set Task count
 }
 reloadTasks()
-createTask()
+saveTask()
